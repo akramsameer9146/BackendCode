7963fc92a6a07012e5b5f797ea03246816277f1e
akramsameer9146 <akramsameer9146@gmail.com>
2023-05-03 12:48:08 +0530

    first commit

---
+++ AddmissionServer/src/main/java/com/sppu/projectname/app/AddmissionServerApplication.java
@@ -0,0 +1,13 @@
+package com.sppu.projectname.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddmissionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddmissionServerApplication.class, args);
+	}
+
+}
---
+++ AddmissionServer/src/test/java/com/sppu/projectname/app/AddmissionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sppu.projectname.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddmissionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
